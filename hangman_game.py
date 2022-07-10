e0a22912b6010dfa7d80f5072ee41e1cb9fb7c55
Hector Rafael <Hector.quine@unmsm.edu.pe>
2022-07-10 14:06:15 -0500

    Menu terminado

--- hangman_game.py
+++ hangman_game.py
@@ -1,6 +1,34 @@
+import encodings
+import os 
+
 def run():
-    pass
+    print("funciona")
+
+def menu():
+    print(" 1.Iniciar Juego \n 2.Creador \n 3.Cerrar Juego")
+    opcion = int(input("Por Favor Eliga una opción: "))
+    try:
+        if opcion == 1:
+            os.system("clear")
+            run()
+
+        elif opcion ==2:
+            os.system("clear")
+            print("Hector Rafael")
+
+        elif opcion ==3: 
+            os.system("clear")
+            print("Thanks ú! ")
+            quit()
+
+        else:
+            raise ValueError("Inserte un valor válido")
+    except ValueError as ve:
+        os.system("clear")
+        print(ve)
+        menu()
+        
 
 
 if __name__ == "__main__":
-    run()
+    menu()
